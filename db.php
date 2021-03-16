a05c55bf25e41bfdeea0d7b1901b53c6c300181b
Branden-Yoshaev <bsyoshae@lakeheadu.ca>
2021-03-16 16:52:11 -0400

    Validation page

--- db.php
+++ db.php
@@ -1,9 +1,6 @@
 <?php
 //AWS
-//$db = new PDO('mysql:host=172.31.22.43;dbname=Branden1137913', 'Branden1137913', 'Lk0ULGu41Y');
-
 // local
-$db = new PDO('mysql:host=localhost;dbname=Branden1137913', 'Branden1137913', 'Lk0ULGu41Y');
+$db = new PDO('mysql:host=172.31.22.43;dbname=Branden1137913', 'Branden1137913', 'Lk0ULGu41Y');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
 ?>
